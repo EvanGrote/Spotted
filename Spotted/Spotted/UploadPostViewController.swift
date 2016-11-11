8d338e61258b01182c73c87757efdaeb0692cf83
Chris Boswell <cboz718@me.com>
2016-11-11 00:28:58 -0600

    Add ViewController for upload tab

---
+++ Spotted/Spotted/UploadPostViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UploadPostViewController.swift
+//  Spotted
+//
+//  Created by Christopher Boswell on 11/9/16.
+//  Copyright © 2016 Christopher Boswell. All rights reserved.
+//
+
+import UIKit
+
+class UploadPostViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+

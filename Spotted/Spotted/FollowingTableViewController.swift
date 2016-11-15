e18d0f022ee960cd548cd266649c7853f69aa8e4
EvanGrote <evanlgrote@gmail.com>
2016-11-15 02:00:27 -0600

    Added UserPost class and began UserPost feed implementation

---
+++ Spotted/Spotted/FollowingTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FollowingTableViewController.swift
+//  Spotted
+//
+//  Created by Evan Grote on 11/15/16.
+//  Copyright © 2016 Evan Grote. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class FollowingTableViewController: UITableViewController {
+    
+    var userPosts: [UserPost] = []
+    var ref = FIRDatabase.database().reference()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Spotted/Spotted/UserPost.swift
@@ -0,0 +1,31 @@
+//
+//  UserPost.swift
+//  Spotted
+//
+//  Created by Evan Grote on 11/15/16.
+//  Copyright © 2016 Evan Grote. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class UserPost {
+    var description: String
+    var tags: [String]
+    var user: String
+    var photo: String
+    
+    init(postDescription: String, postTags: [String], posterId: String, postPhoto: String) {
+        self.description = postDescription
+        self.tags = postTags
+        self.user = posterId
+        self.photo = postPhoto
+    }
+    
+    init(snapshot: FIRDataSnapshot) {
+        self.description = snapshot.value(forKey: "description") as! String
+        self.tags = snapshot.value(forKey: "tag") as! [String]
+        self.user = snapshot.value(forKey: "user") as! String
+        self.photo = snapshot.value(forKey: "userPhoto") as! String
+    }
+}

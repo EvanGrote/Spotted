bd5c8f2fbb257775b7ab50dda26ab8812bdaaabd
Chris Boswell <cboz718@me.com>
2016-11-15 14:52:56 -0600

    Create around me ViewController and initial map

---
+++ Spotted/Spotted/AroundMeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Spotted
+//
+//  Created by Evan Grote on 10/25/16.
+//  Copyright © 2016 Evan Grote. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class AroundMeViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+

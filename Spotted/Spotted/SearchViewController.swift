02424c36d32e7adae9e96143886af7d48ce1c598
Christopher Boswell <cboz718@me.com>
2016-11-27 02:20:04 -0600

    add initial SearchViewController.swift class

---
+++ Spotted/Spotted/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SearchViewController.swift
+//  Spotted
+//
+//  Created by Christopher Boswell on 11/27/16.
+//  Copyright © 2016 Evan Grote, Christopher Boswell. All rights reserved.
+//
+
+import UIKit
+
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}

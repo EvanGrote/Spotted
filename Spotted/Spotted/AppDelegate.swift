561d77dab7a6a83294751af6aa25c7fe8da0b816
Christopher Boswell <cboz718@me.com>
2016-12-11 05:52:06 -0600

    add userdefault app delegate options

--- Spotted/Spotted/AppDelegate.swift
+++ Spotted/Spotted/AppDelegate.swift
@@ -25,6 +25,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("setting userdefaults")
+        print(StaticVariables.followingTags)
+        print(StaticVariables.followingTagsCount)
+        userDefault.set(StaticVariables.followingTags, forKey: "followingTags")
+        userDefault.set(StaticVariables.followingTagsCount, forKey: "followingTagsCount")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -39,6 +44,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("setting userdefaults")
+        print(StaticVariables.followingTags)
+        print(StaticVariables.followingTagsCount)
+        userDefault.set(StaticVariables.followingTags, forKey: "followingTags")
+        userDefault.set(StaticVariables.followingTagsCount, forKey: "followingTagsCount")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
